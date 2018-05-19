a9eabddf8405856b64c8c693d98f97ea6cd50a6d
Jake Fenske <jfenske5688@gmail.com>
2018-05-19 00:17:54 -0600

    Completed code for Challenge 3

--- Challenge3/Challenge3.py
+++ Challenge3/Challenge3.py
@@ -1,11 +1,27 @@
-import urllib2
+from collections import defaultdict
 
 def main():
-    response = urllib2.urlopen(challengeURL)
-    pageSource = response.read()
+    stringFile = "Challenge3.txt"
+    inputString = ""
 
-    print(pageSource)
+    with open(stringFile, 'rb') as f:
+        inputString = f.read()
+
+    charCountDct = defaultdict(int)
+
+    for char in inputString:
+        charCountDct[char] += 1
+
+    rarestCount = len(inputString)
+
+    for key, val in charCountDct.iteritems():
+        if val < rarestCount: rarestCount = val
+
+    rareChars = []
+    for key, val in charCountDct.iteritems():
+        if val == rarestCount: rareChars.append(key)
+
+    print "".join(rareChars)
 
 if __name__ == "__main__":
-    challengeURL = "http://www.pythonchallenge.com/pc/def/ocr.html"
     main()
